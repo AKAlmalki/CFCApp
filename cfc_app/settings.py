9f3794de545b8d0aa115182c8c49db95cc372e62
AKAlmalki <ahmed.k.1454@gmail.com>
2023-11-28 11:22:57 +0300

    modify the name of static files folder and ignore it for git

--- cfc_app/settings.py
+++ cfc_app/settings.py
@@ -149,7 +149,7 @@ STATIC_URL = 'static/'
 #         os.path.join(BASE_DIR, 'web/static'),
 # ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
