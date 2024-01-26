bd565a900766b18a37a070f97b2d495e29756be7
AKAlmalki <ahmed.k.1454@gmail.com>
2024-01-26 17:39:46 +0300

    uncomment a check condition on beneficiary request update

--- myapp/views.py
+++ myapp/views.py
@@ -1715,21 +1715,21 @@ def beneficiary_request_update(request, user_id):
         # Get the last added beneficiary_request
 
         # # Check if there's existing beneficiary requests
-        # is_beneficiary_request = Beneficiary_request.objects.filter(
-        #     user=user).exists()
+        is_beneficiary_request = Beneficiary_request.objects.filter(
+            user=user).exists()
 
-        # if is_beneficiary_request:
-        #     last_beneficiary_request = Beneficiary_request.objects.latest(
-        #         'created_at')
-        #     if last_beneficiary_request.request_type == "update":
-        #         messages.error(
-        #             request, "لديك طلب تحديث بيانات سابق! لا يمكنك طلب تحديث بيانات جديد الى ان تتغير حالة الطلب السابق.")
-        #         return redirect('home')
+        if is_beneficiary_request:
+            last_beneficiary_request = Beneficiary_request.objects.latest(
+                'created_at')
+            if last_beneficiary_request.request_type == "update":
+                messages.error(
+                    request, "لديك طلب تحديث بيانات سابق! لا يمكنك إنشاء طلب جديد الى ان يتم الرد من قبل فريق العمل على الطلب الأخير.")
+                return redirect('home')
 
-        # else:
-        #     messages.error(
-        #         request, "ليس لديك طلبات سابقة!!")
-        #     return redirect('home')
+        else:
+            messages.error(
+                request, "ليس لديك طلبات سابقة!!")
+            return redirect('home')
 
         beneficiary_obj = beneficiary.objects.get(
             user=user_id)
