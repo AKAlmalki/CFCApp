121b1891e2340fc476264d3aa4f05c06f934aeee
dawaime <dawai.tech.team@gmail.com>
2024-09-11 17:48:52 +0300

    Update: Make national_id in CustomUser table as nullable column to fix a problem in production

--- myapp/models.py
+++ myapp/models.py
@@ -44,7 +44,7 @@ class CustomUser(AbstractUser):
     gender = models.CharField(max_length=5, null=True)
     phonenumber = models.CharField(max_length=15, null=True, unique=True)
     last_updated = models.DateField(null=True)
-    national_id = models.CharField(max_length=20, default=0, unique=True)
+    national_id = models.CharField(max_length=20, null=True, unique=True)
     nationality = models.CharField(max_length=64, null=True)
     email = models.EmailField(unique=True)  # Override the email field
     # add additional field in here
