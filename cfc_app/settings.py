c5ed78ca0e16917ac3bbda67490c7a9c10d19451
oussama <oussama.hel@gmail.com>
2025-10-26 09:13:51 +0100

    use environment variables for DEBUG and ALLOWED_HOSTS settings

--- cfc_app/settings.py
+++ cfc_app/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['port433.link', 'www.port433.link', 'https://port433.link', '34.89.34.177', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 CSRF_TRUSTED_ORIGINS = [
     'https://beneficiaries.asdiqa.org.sa'
